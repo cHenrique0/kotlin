5b9b6caa535106de95f3f919aa1289651377d761
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-05-17 23:15:07 -0300

    learning about variables

---
+++ Classes/src/main/kotlin/01.Variables.kt
@@ -0,0 +1,19 @@
+fun main() {
+    // 1. Variáveis:
+
+    // 1.1. Declarando variáveis multáveis
+    var myFirstVariable = "Minha primeira variável"
+    println(myFirstVariable)
+
+    // Reatribuindo valor à variável
+    myFirstVariable = "Dando outro valor à variável"
+    println(myFirstVariable)
+
+    // 1.2. Declarando variáveis imultáveis
+    val myFirstImutableVariable = 0;
+    println(myFirstImutableVariable)
+
+    // Tentando reatribuir
+    // myFirstImutableVariable = 20 // Não é possível reatribuir a variável
+    // println(myFirstImutableVariable)
+}
