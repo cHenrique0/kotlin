245bfea4fd34714643cd4915ce888bbda0b4c861
Claudio Henrique <claudio_henrique20@hotmail.com>
2024-05-21 11:31:38 -0300

    template string

---
+++ Classes/src/main/kotlin/03.TextFormatting.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    // 1. Concatenação
+    val name1 = "Maria"
+    val age1 = 31
+    println("Olá, " + name1 + ". Sua idade é " + age1)
+
+    // 2. Template String
+    val name2 = "João"
+    val age2 = 28
+    println("Olá. $name2. Sua idade é $age2")
+}
